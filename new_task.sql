056a9785dfb4926d6a7d7b4bb5165db0eccd44f1
HAMID <abdulhamidabari652@gmail.com>
2025-04-10 09:36:03 +0100

    new task

---
+++ new_task.sql
@@ -0,0 +1,5 @@
+select * from CUSTOMER,INVOICE,INVOICE_ITEM; 
+select LastName,FirstName,Phone from Customer;
+select LastName,FirstName,Phone from Customer where FirstName = 'Nikki'; 
+select LastName,FirstName,Phone,DateIn,DateOut from customer,invoice where TotalAmount > 100.00;
+select LastName, FirstName, Phone from Customer where FirstName like 'B%';
