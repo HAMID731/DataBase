07600111278b5be2e6b87e57f16487849c5e84ff
HAMID <abdulhamidabari652@gmail.com>
2025-04-10 14:19:14 +0100

    new

--- new_task.sql
+++ new_task.sql
@@ -3,3 +3,4 @@ select LastName,FirstName,Phone from Customer;
 select LastName,FirstName,Phone from Customer where FirstName = 'Nikki'; 
 select LastName,FirstName,Phone,DateIn,DateOut from customer,invoice where TotalAmount > 100.00;
 select LastName, FirstName, Phone from Customer where FirstName like 'B%';
+select lastName, FirstName, Phone from Customer where LastName like '%cat%';
